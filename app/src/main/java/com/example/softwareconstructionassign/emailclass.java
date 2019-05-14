449ebb5e53378afce09032852dd75d1d32d2f1a5
Dileep Vemula <u6631257@anu.edu.au>
2019-05-14 19:32:48 +1000

    login registrartion done. DB write successful

---
+++ app/src/main/java/com/example/softwareconstructionassign/emailclass.java
@@ -0,0 +1,49 @@
+package com.example.softwareconstructionassign;
+
+public class emailclass {
+    String id;
+    String email;
+    String password;
+
+    public emailclass(){
+    }
+
+    public emailclass(String id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "emailclass{" +
+                "id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- app/src/main/java/com/example/softwareconstructionassign/login.java
+++ app/src/main/java/com/example/softwareconstructionassign/login.java
@@ -1,24 +1,78 @@
 package com.example.softwareconstructionassign;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class login extends AppCompatActivity {
+
+
+
+    Button login;
+    Button signUp;
+    EditText email, password;
+
+    DatabaseReference dblogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        dblogin = FirebaseDatabase.getInstance().getReference("users");
+
+        login = findViewById(R.id.buttonLogin);
+        signUp = findViewById(R.id.buttonSignUp);
+        email = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.textPassword);
+
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent =new Intent(getBaseContext(),mainscreen.class);
+                startActivity(myIntent);
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addemail();
+            }
+        });
+
     }
 
-    public void buttonPress(View v){
-        Intent intent = new Intent(getBaseContext(), mainscreen.class);
-        EditText editText = (EditText) findViewById(R.id.textPassword);
-        String message = editText.getText().toString();
-        startActivity(intent);
 
+
+    private void addemail(){
+        String emailaddress = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+
+        if(!emailaddress.isEmpty()){
+
+            String id = emailaddress.split("@")[0];
+
+            emailclass useremail = new emailclass(id,emailaddress,pass);
+            dblogin.child(id).setValue(useremail);
+            Toast.makeText(this,"Inserted", Toast.LENGTH_LONG).show();
+
+            email.setText("");
+            password.setText("");
+
+        }
+        else {
+            Toast.makeText(this,"Please enter a email address", Toast.LENGTH_LONG).show();
+        }
     }
 }
