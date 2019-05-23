f95685dda158fccf5eea7ee68b78067e5962341e
Dileep Vemula <u6631257@anu.edu.au>
2019-05-24 02:50:52 +1000

    About file updated.

--- app/src/main/java/com/example/softwareconstructionassign/About.java
+++ app/src/main/java/com/example/softwareconstructionassign/About.java
@@ -1,11 +1,14 @@
 package com.example.softwareconstructionassign;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
 
@@ -14,6 +17,18 @@ public class About extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         this.setTitle("About Us");
+        ImageView imageView = findViewById(R.id.imageView);
+        TextView assign = findViewById(R.id.textView);
+        TextView version = findViewById(R.id.textView9);
+        TextView developers = findViewById(R.id.textView10);
+        TextView app = findViewById(R.id.textView11);
+        String src = "Developed as part of Assignment of "+ "<b>" + "COMP6442"+ "</b>";
+        assign.setText(Html.fromHtml(src));
+
+        version.setText("version 1.0");
+        String src2 = "<b>"+"Developed by : "+"</b>"+"\"Abhishek Chetri, Mansoor Ali Halari, Kruthi Senapathi and Dileep Vemula.";
+        developers.setText(Html.fromHtml(src2));
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
